13243b03509b77ad4c637291e1efefba1e5238c0
holla394 <holla394@umn.edu>
2023-03-19 22:48:11 -0500

    added zoom control to map

--- webapp/static/js/mapLogic.js
+++ webapp/static/js/mapLogic.js
@@ -14,16 +14,48 @@ let plot_object = {
 }; 
 let myMap = L.map("map", {
     center: coordinates,
-    zoom: 5
+    zoom: 5,
+    zoomControl: true
 });
-
 
 // Adding the tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
+myMap.scrollWheelZoom.disable();
 
+L.Control.textbox = L.Control.extend({
+    onAdd: function(map) {
+        let text = L.DomUtil.create('div');
+        text.id = "info_text";
+        text.innerHTML = "<strong>Hold CTRL to Zoom</strong>"
+        return text;
+    },
+
+    onRemove: function(map) {
+        // Nothing to do here
+    }
+});
+L.control.textbox = function(opts) { return new L.Control.textbox(opts);}
+L.control.textbox({ position: 'bottomleft' }).addTo(myMap);
+
+window.addEventListener("keyup", (e) => {
+    if (!e.ctrlKey) {
+        log(e);
+        myMap.scrollWheelZoom.disable();
+    }
+});
+window.addEventListener("keydown", (e) => {
+    if (e.ctrlKey) {
+        log(e);
+        myMap.scrollWheelZoom.enable();
+    }
+});
+
+function log(event){
+  console.log( event.type );
+}
 
 function truncateString(str, num) {
     if (str.length > num) {
@@ -65,3 +97,25 @@ function loadpage() {
 };
 
 loadpage();
+
+// let pc = true;
+// if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+//   pc = false;
+// }
+// var map1 = L.map("map", {
+//   center: [39.50, -98.35],
+//   zoom: 5,
+//   dragging: pc,
+//   tap: pc
+// });
+
+// const mapEl = document.querySelector("#map");
+// mapEl.addEventListener("touchstart", onTwoFingerDrag);
+// mapEl.addEventListener("touchend", onTwoFingerDrag);
+// function onTwoFingerDrag (e) {
+//   if (e.type === 'touchstart' && e.touches.length === 1) {
+//     e.currentTarget.classList.add('swiping')
+//   } else {
+//     e.currentTarget.classList.remove('swiping')
+//   }
+// }
