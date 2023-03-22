6d6d48ffa1285b116c43d4f49fb64b79ec3282b1
holla394 <holla394@umn.edu>
2023-03-21 19:15:43 -0500

    adding cors to app

--- webapp/app.py
+++ webapp/app.py
@@ -1,6 +1,6 @@
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from werkzeug.serving import run_simple
-
+from flask_cors import CORS
 from currentbillsdashboard import app as app1
 from topbillsdashboard import app as app2
 from flask_app import flask_app
@@ -9,6 +9,6 @@ application = DispatcherMiddleware(flask_app, {
     '/currentbillsdashboard': app1.server,
     '/topbillsdashboard': app2.server
 })
-
+cors = CORS(application)
 if __name__ == '__main__':
     run_simple('localhost', 8050, application, use_reloader=False)
