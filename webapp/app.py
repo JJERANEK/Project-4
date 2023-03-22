51710e9141a7ca728a8b3180a7c8e1fd2f52fa18
holla394 <holla394@umn.edu>
2023-03-21 20:11:28 -0500

    final cleanup of app: changed dash table probability formats and added flask CORS

--- webapp/app.py
+++ webapp/app.py
@@ -1,14 +1,15 @@
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from werkzeug.serving import run_simple
-from flask_cors import CORS
 from currentbillsdashboard import app as app1
 from topbillsdashboard import app as app2
 from flask_app import flask_app
+
 
 application = DispatcherMiddleware(flask_app, {
     '/currentbillsdashboard': app1.server,
     '/topbillsdashboard': app2.server
 })
-# cors = CORS(application)
+
+
 if __name__ == '__main__':
     run_simple('0.0.0.0', 8050, application, use_reloader=True)
